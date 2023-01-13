db2dd05b6deb3941e9c508ba2e87db6f6b149fd3
Gabe Morris <gabemorris1042@gmail.com>
2023-01-13 15:38:35 -0400

    created the html generation for the manager,engineer, and intern cards

--- index.js
+++ index.js
@@ -146,7 +146,79 @@ function internPrompts() {
 }
 
 function generateHTML(input) {
-  console.log(input)
+
+  if (input.engineers) {
+    // use var to declare variable outside of this if block as well
+    var engineerHTML = '';
+    input.engineers.forEach(item => {
+      engineerHTML = engineerHTML.concat(`
+  <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${item.engineerName}</h5>
+      <p class="card-text">Employee id: ${item.engineerId}</p>
+      <p class="card-text">${item.engineerEmail}</p>
+      <p class="card-text">Github: ${item.engineerGithub}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+  `)
+    })
+  }
+
+  if (input.interns) {
+    // use var to declare variable outside of this if block as well
+    var internHTML = '';
+    input.interns.forEach(item => {
+      internHTML = internHTML.concat(`
+  <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${item.internName}</h5>
+      <p class="card-text">Employee id: ${item.internId}</p>
+      <p class="card-text">${item.internEmail}</p>
+      <p class="card-text">School: ${item.internSchool}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+  `)
+    })
+  }
+
+  let html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <title>Our Team</title>
+</head>
+<body>
+  <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${input.managerName}</h5>
+      <p class="card-text">Employee id: ${input.managerId}</p>
+      <p class="card-text">${input.managerEmail}</p>
+      <p class="card-text">Office Number: ${input.managerOfficeNumber}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+  ${engineerHTML}
+  ${internHTML}
+</body>
+</html>
+`
+
+  fs.writeFile('index.html', html, (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully");
+    }
+  })
 }
 
 managerPrompts();
